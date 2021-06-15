125ecbb3b58c33837c097b5a3eba6570e7c0d244
gallpaul8892 <gallpaul8892@gmail.com>
2021-06-15 23:05:33 +0100

    Just need to finish about section and work out algo for pricing

--- src/ServiceWizard/ServiceWizard.js
+++ src/ServiceWizard/ServiceWizard.js
@@ -1,9 +1,20 @@
 import Modal from "react-modal";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "../Css/ServiceWizard.css";
+import PriceList from "../PriceList";
 
 const ServiceWizard = (props) => {
-  var subtitle;
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [recommendation, setRecommendation] = useState("");
+
+  const slow = useRef();
+  const popups = useRef();
+  const virus = useRef();
+  const noPower = useRef();
+  const keepData = useRef();
+
+  Modal.setAppElement(document.getElementById('root'))
 
   const customStyles = {
     content: {
@@ -15,19 +26,46 @@ const ServiceWizard = (props) => {
       transform: "translate(-50%, -50%)",
     },
   };
-  const [modalIsOpen, setIsOpen] = useState(false);
-  function openModal() {
-    setIsOpen(true);
-  }
 
-  function afterOpenModal() {
+  const prices = {
+    virusRemoval: 50,
+    osInstall: 30,
+    keepData: 10,
+    keepApps: 10,
+    ssdUpgrade: 30,
+  };
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const afterOpenModal = () => {
     // references are now sync'd and can be accessed.
     subtitle.style.color = "#f00";
-  }
+  };
 
-  function closeModal() {
+  const closeModal = () => {
     setIsOpen(false);
-  }
+  };
+
+  const recommendationCost = () => {
+    let cost = 0;
+    if (slow.current.checked) {
+      cost += 30;
+    }
+    if (popups.current.checked) {
+      cost += prices.virusRemoval;
+    }
+    if (keepData.current.checked) {
+      cost += prices.keepData;
+    }
+    if(noPower.current.checked){
+        cost = "Power failure is not an issue that the computer mechanic can solve."
+        return setRecommendation(cost);
+    }
+    
+    return setRecommendation("£" + cost);
+  };
 
   return (
     <div className="service-wizard">
@@ -39,18 +77,35 @@ const ServiceWizard = (props) => {
         style={customStyles}
         contentLabel="Example Modal"
       >
-        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>PC Service Wizard</h2>
+        <h3>Use this wizard to get your PC Service quote.</h3>
+
+        <div>
+          <div>
+            Slow <input type="checkbox" ref={slow} onChange={recommendationCost}></input>
+          </div>
+          <div>
+            Always getting popups <input type="checkbox" ref={popups} onChange={recommendationCost}></input>
+          </div>
+          <div>
+            Virus <input type="checkbox" ref={virus} onChange={recommendationCost}></input>
+          </div>
+          <div>
+            Won't turn on <input type="checkbox" ref={noPower} onChange={recommendationCost}></input>
+          </div>
+
+          <div>
+            Worried about losing data{" "}
+            <input type="checkbox" ref={keepData} onChange={recommendationCost}></input>
+          </div>
+        </div>
+        <div>
+          <h3>Recommendation</h3>
+          {recommendation}
+        </div>
         <button onClick={closeModal} className="service-wizard-btn">
-          close
+          Cancel
         </button>
-        <div>I am a modal</div>
-        <form>
-          <input />
-          <button>tab navigation</button>
-          <button>stays</button>
-          <button>inside</button>
-          <button>the modal</button>
-        </form>
       </Modal>
     </div>
   );
