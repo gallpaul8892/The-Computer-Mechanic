50a29817bb84613d4703e509169cd16dddcbd792
gallpaul8892 <gallpaul8892@gmail.com>
2021-06-16 20:06:16 +0100

    Removing title from App

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <title>Computer Mechanic - Glasgow</title>
       <Header />
       <Router>
         <menu>
