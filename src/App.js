da58dc19dcd7c54053783317b2aea8c44638b3c8
gallpaul8892 <gallpaul8892@gmail.com>
2021-06-16 20:06:03 +0100

    Tidy up and update title

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
     <div className="App">
+      <title>Computer Mechanic - Glasgow</title>
       <Header />
       <Router>
         <menu>
