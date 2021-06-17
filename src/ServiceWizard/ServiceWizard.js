46880f466b75228b6ffdb31e7492bed50c08fee7
gallpaul8892 <gallpaul8892@gmail.com>
2021-06-18 00:01:47 +0100

    Service Wizard work in progress

--- src/ServiceWizard/ServiceWizard.js
+++ src/ServiceWizard/ServiceWizard.js
@@ -1,9 +1,16 @@
 import Modal from "react-modal";
 import { useState, useRef } from "react";
+import { Button } from "@material-ui/core";
+import {
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  FormControl,
+} from "@material-ui/core";
+import StepWizard from "react-step-wizard";
 import "../Style/Css/ServiceWizard.css";
 
 const ServiceWizard = (props) => {
-  let subtitle;
   const [modalIsOpen, setIsOpen] = useState(false);
   const [recommendation, setRecommendation] = useState("");
 
@@ -13,7 +20,7 @@ const ServiceWizard = (props) => {
   const noPower = useRef();
   const keepData = useRef();
 
-  Modal.setAppElement(document.getElementById('root'))
+  Modal.setAppElement(document.getElementById("root"));
 
   const customStyles = {
     content: {
@@ -23,24 +30,19 @@ const ServiceWizard = (props) => {
       bottom: "auto",
       marginRight: "-50%",
       transform: "translate(-50%, -50%)",
+      padding: 5,
     },
   };
 
   const prices = {
-    virusRemoval: 50,
+    virusRemoval: 30,
     osInstall: 30,
     keepData: 10,
     keepApps: 10,
-    ssdUpgrade: 30,
   };
 
   const openModal = () => {
     setIsOpen(true);
-  };
-
-  const afterOpenModal = () => {
-    // references are now sync'd and can be accessed.
-    subtitle.style.color = "#f00";
   };
 
   const closeModal = () => {
@@ -49,6 +51,7 @@ const ServiceWizard = (props) => {
 
   const recommendationCost = () => {
     let cost = 0;
+
     if (slow.current.checked) {
       cost += 30;
     }
@@ -58,56 +61,130 @@ const ServiceWizard = (props) => {
     if (keepData.current.checked) {
       cost += prices.keepData;
     }
-    if(noPower.current.checked){
-        cost = "Power failure is not an issue that the computer mechanic can solve."
-        return setRecommendation(cost);
-    }
-    
+
     return setRecommendation("£" + cost);
   };
 
   return (
-    <div className="service-wizard">
-      <button onClick={openModal}>PC Service Wizard</button>
+    <>
+      <Button color="primary" variant="contained" onClick={openModal}>
+        PC Service Wizard
+      </Button>
       <Modal
         isOpen={modalIsOpen}
-        onAfterOpen={afterOpenModal}
         onRequestClose={closeModal}
         style={customStyles}
         contentLabel="Example Modal"
+        className="service-wizard-modal"
       >
-        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>PC Service Wizard</h2>
-        <h3>Use this wizard to get your PC Service quote.</h3>
-
-        <div>
-          <div>
-            Slow <input type="checkbox" ref={slow} onChange={recommendationCost}></input>
-          </div>
-          <div>
-            Always getting popups <input type="checkbox" ref={popups} onChange={recommendationCost}></input>
-          </div>
-          <div>
-            Virus <input type="checkbox" ref={virus} onChange={recommendationCost}></input>
-          </div>
-          <div>
-            Won't turn on <input type="checkbox" ref={noPower} onChange={recommendationCost}></input>
-          </div>
-
-          <div>
-            Worried about losing data{" "}
-            <input type="checkbox" ref={keepData} onChange={recommendationCost}></input>
-          </div>
-        </div>
-        <div>
-          <h3>Recommendation</h3>
-          {recommendation}
-        </div>
-        <button onClick={closeModal} className="service-wizard-btn">
-          Cancel
-        </button>
+        <Wizard
+          slow={slow}
+          recommendationCost={recommendationCost}
+          popups={popups}
+          virus={virus}
+          recommendation={recommendation}
+          closeModal={closeModal}
+          noPower={noPower}
+          keepData={keepData}
+        ></Wizard>
       </Modal>
+    </>
+  );
+};
+
+const Wizard = (props) => {
+  return (
+    <div className="service-wizard">
+      <h3>Use this wizard to get your PC Service quote.</h3>
+
+      <div className="wizard-body">
+        <StepWizard>
+          <Slower />
+          <Recommendation />
+        </StepWizard>
+        <Button
+          color="secondary"
+          variant="contained"
+          onClick={props.closeModal}
+          className="cancel-btn"
+        >
+          Close
+        </Button>
+      </div>
     </div>
   );
+};
+
+const Slower = (props) => {
+  const [value, setValue] = useState("Yes");
+
+  return (
+    <div>
+      <div>
+        <span>Is your PC slower than the day you bought it?</span>
+      </div>
+      <div>
+        <RadioGroup
+          aria-label="slow"
+          name="slow"
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+            console.log(e.target.value);
+          }}
+        >
+          <FormControlLabel value="No" control={<Radio />} label="No" />
+          <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
+        </RadioGroup>
+      </div>
+      <div>
+        <Button color="secondary" variant="contained" onClick={props.prevStep}>
+          Previous
+        </Button>
+        <Button color="primary" variant="contained" onClick={props.nextStep}>
+          Next
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+const Slower = (props) => {
+  const [value, setValue] = useState("Yes");
+
+  return (
+    <div>
+      <div>
+        <span>Is your PC slower than the day you bought it?</span>
+      </div>
+      <div>
+        <RadioGroup
+          aria-label="slow"
+          name="slow"
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+            console.log(e.target.value);
+          }}
+        >
+          <FormControlLabel value="No" control={<Radio />} label="No" />
+          <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
+        </RadioGroup>
+      </div>
+      <div>
+        <Button color="secondary" variant="contained" onClick={props.prevStep}>
+          Previous
+        </Button>
+        <Button color="primary" variant="contained" onClick={props.nextStep}>
+          Next
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+const Recommendation = () => {
+  return <div></div>;
 };
 
 export default ServiceWizard;
