d24c29f2374fc15787293cafeb3c9ed46d41ceff
gallpaul8892 <gallpaul8892@gmail.com>
2021-06-18 18:04:00 +0100

    added some links and removed service wizard for now

--- src/About/About.js
+++ src/About/About.js
@@ -1,12 +1,34 @@
-const About = ()=>{
-    return<div>
-        <h1>
-            About The Computer Mechanic
-        </h1>
-        <p>
-            Coming Soon...
-        </p>
+import "../Style/Css/About.css";
+
+import { Link } from "react-router-dom";
+
+
+const About = () => {
+  return (
+    <div className="about">
+      <p>
+        With over 9 years experience in PC repairs, we are IT Professionals who
+        want to help people to get the most out of their aging laptops and
+        computers.
+      </p>
+      <p>
+        Our reasons are two fold, firstly we're a business so naturallly we want
+        to help our customers save money by avoiding spending money on new
+        devices that aren't required.
+      </p>
+      <p>
+        Additionally, when you throw your device away there is an environmental
+        impact. As you can imagine 10s of millions of PCs and laptops are being
+        sold every year, by prolonging the lives of these devices, we can make a
+        small difference
+      </p>
+      <p>
+        There are many things that you can do to increase the lifespan of a device,
+        get in touch if you want some advice, or <Link to="/services">view our Services</Link> and get
+        your device booked in.
+      </p>
     </div>
-}
+  );
+};
 
 export default About;
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,4 +1,3 @@
-import logo from "../spanner.svg";
 import "../Style/Css/Header.css";
 
 const Header = () => {
@@ -6,7 +5,8 @@ const Header = () => {
     <header className="App-header">
       <div>
         <h1>The Computer Mechanic</h1>
-        <h2>Glasgow</h2>
+        <h3>Glasgow and Surrounding Areas</h3>
+        <h3><a href="mailto:computermech@gmail.com">computer.mech.gla@gmail.com</a> | <a href="tel:07943572154">07943572154</a></h3>
       </div>
     </header>
   );
