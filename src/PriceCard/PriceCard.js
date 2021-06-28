84ada14816db168ec8e731cd8a23871c0c04990e
gallpaul8892 <gallpaul8892@gmail.com>
2021-06-29 00:12:44 +0100

    Updating benefits

--- src/PriceCard/PriceCard.js
+++ src/PriceCard/PriceCard.js
@@ -8,6 +8,15 @@ const PriceCard = (props) => {
         <strong>{service.title}</strong>
       </h3>
       <p>{service.description}</p>
+      {service.benefits && service.benefits.length > 0 && (
+        <div>
+          <h3>Benefits</h3>
+          {service.benefits.map((benefit) => (
+            <div>- {benefit}</div>
+          ))}
+        </div>
+      )}
+
       {service.price && <h3>£{service.price}</h3>}
       {service.time && <p>{serviceLevelWording(service.time)}</p>}
     </div>
--- src/config/appData.js
+++ src/config/appData.js
@@ -11,6 +11,7 @@ const services = [
     description:
       "If you we spend time looking at your device and make a recommendation that you do not wish to proceed with, we will request a £10 fee to cover our time.",
     price: 10,
+    benefits: ["Save money on a new PC by exploring your options"],
   },
   {
     id: "fullReset",
@@ -19,6 +20,12 @@ const services = [
       "Recommended for PCs that have became slow over time. Data will be safe but applications will need to be reinstalled by owner.",
     price: 30,
     time: 24,
+    benefits: [
+      "Remove all applications",
+      "Laptop will run like new",
+      "Remove Viruses",
+      "Keep Data",
+    ],
   },
   {
     id: "virusRemoval",
@@ -27,6 +34,7 @@ const services = [
       "Recommended if your PC has been infected with a virus, including popups whilst browsing. Applications and Data will be retained, if applications are not essential then you should opt for the Data-Safe Reset.",
     price: 50,
     time: 48,
+    benefits: ["Remove Viruses", "Keep Data", "Keep Applications"],
   },
   {
     id: "hardDriveUpgrade",
@@ -35,6 +43,13 @@ const services = [
       "Recommended if your PC or Laptop is still slow after a service, or if you are running out of disk space. Price excludes cost of new hard drive. Application and Data will be retained.",
     price: 50,
     time: 48,
+    benefits: [
+      "Upto 10x faster start up times",
+      "Games & Apps load faster",
+      "More space",
+      "Longer lasting",
+      "Less power consumption",
+    ],
   },
   {
     id: "other",
