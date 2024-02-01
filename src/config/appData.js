381b07e524ad30c6c4b0c8b1a1259caf0c7e9afe
gallpaul8892 <gallpaul8892@gmail.com>
2024-02-01 08:16:37 +0000

    updating email

--- src/config/appData.js
+++ src/config/appData.js
@@ -1,7 +1,7 @@
 //Header
 const title = "The Computer Mechanic";
 const tagLine = "Glasgow and Surrounding Areas";
-const emailAddress = "computermech@gmail.com";
+const emailAddress = "computer.mech.gla@gmail.com";
 const telephoneNumber = "07341496642";
 
 export { emailAddress, telephoneNumber, title, tagLine };
